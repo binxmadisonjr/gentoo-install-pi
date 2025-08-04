407bb6c10dd282285cabd80e682253bcdf90506c
k ostaN <pyotrthevampire@gmail.com>
2025-08-04 14:38:31 -0500

    Create 01_package_check.sh

---
+++ source/01_package_check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+source "../config.env"
+
+# Check for requred packages before starting install
+for cmd in $REQUIRED_CMDS; do
+    if ! command -v $cmd &>/dev/null; then
+        echo "Missing required command: $cmd"
+        MISSING=1
+    fi
+done
+
+if [[ $MISSING -eq 1 ]]; then
+    echo "Please install all required packages before running this script."
+    exit 1
+fi
