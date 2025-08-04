fea82fe627b8f6f990e9e1b1a54aff5ad8726e12
k ostaN <pyotrthevampire@gmail.com>
2025-08-04 14:55:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,27 +4,9 @@ set -e
 # Load config
 source "config.env"
 
-# Run package check
+# Run install
 "source/01_package_check.sh"
-
-
-
-# Unmount any existing mounts (be careful)
-umount ${DEVICE}* || true
-
-# Create partition table: 512MB FAT32 boot, rest BTRFS root
-parted --script $DEVICE \
-    mklabel msdos \
-    mkpart primary fat32 1MiB 513MiB \
-    mkpart primary btrfs 513MiB 100% \
-    set 1 boot on
-    set 1 lba on
-
-# Format partitions
-mkfs.vfat -F 32 -n BOOT ${DEVICE}1
-mkfs.btrfs -f -L ROOTFS ${DEVICE}2
-
-# Create and mount to working dir
-mkdir -p /mnt/rootfs /mnt/rootfs/boot
-mount ${DEVICE}2 /mnt/rootfs
-mount ${DEVICE}1 /mnt/rootfs/boot
+echo "Package check is complete!"
+echo "Starting downloads now!"
+"source/02_downloads.sh"
+echo "Downloads complete!"
