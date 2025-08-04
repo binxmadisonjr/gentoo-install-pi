9fb1fba65a568cfbceae2f15b705c1988a7e4984
k ostaN <pyotrthevampire@gmail.com>
2025-08-04 15:31:56 -0500

    Update 03_setup.sh

--- source/03_setup.sh
+++ source/03_setup.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -e 
+
+# --- Load config ---
 source "config.env"
 
-# Disk setup
-echo "Unmounting any existing mounts (be careful!)"
+# --- Disk setup ---
+echo "Unmounting any existing mounts..."
+
 umount ${DEVICE}1 || true
 umount ${DEVICE}2 || true
 
-echo "Partitioning and formatting drive..."
+echo "Unmounted any existing partitions succesfully!"
+
+# --- Create partitions ---
+echo "Creating partitions..."
+
 parted --script $DEVICE \
     mklabel msdos \
     mkpart primary fat32 1MiB 513MiB \
@@ -15,32 +22,51 @@ parted --script $DEVICE \
     set 1 boot on \
     set 1 lba on
 
+echo "Partitions created successfully" 
+
+# --- Format partitions ---
+echo "Formatting partitions..."
+
 mkfs.vfat -F 32 -n boot ${DEVICE}1
 mkfs.btrfs -f -L root ${DEVICE}2
 
+echo "Partitions formatted succesfully!"
+
+# --- Create mountpoints and mount ---
 echo "Creating and mounting partitions..."
+
 mkdir -p /mnt/root /mnt/root/boot
 mount ${DEVICE}2 /mnt/root
 mount ${DEVICE}1 /mnt/root/boot
 
-# Extract base system
+echo "Partitions mounted successfully!"
+
+# --- Extract base system ---
 echo "Extracting stage3 to root..."
+
 tar -xJpf $LATEST -C /mnt/root --xattrs-include='*.*' --numeric-owner
 
+echo "Stage3 extracted to root successfully!"
+
+# --- Extract Pi boot firmware to mountpoint ---
 echo "Extracting Pi boot firmware to boot..."
+
 cp -r firmware/boot/* /mnt/root/boot/
 
+# --- Extract nonfree firmware to mountpoint ---
 echo "Copying nonfree firmware to /lib/firmware..."
+
 cp -r firmware-nonfree/* /mnt/root/lib/firmware/ || true
 
+# --- Extract bluetooth firmware to mountpoint ---
 echo "Copying bluetooth firmware to /lib/firmware..."
 cp -r bluez-firmware/* /mnt/root/lib/firmware/ || true
 
-# Kernel build/install
+# --- Kernel build/install ---
 echo "Building and installing the kernel and modules (this may take a while)..."
 cd linux
 
-# Use Pi default config
+# --- Use Pi default config ---
 KERNEL=kernel8
 make bcm2711_defconfig      # For Raspberry Pi 4/5, adjust for your board if needed
 make -j$(nproc)
