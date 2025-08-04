14383de5cfeac59ed69704783bf4dbdf1c058060
k ostaN <pyotrthevampire@gmail.com>
2025-08-04 15:22:46 -0500

    Update and rename 01_package_check.sh to 01_preflight_check.sh

--- source/01_preflight_check.sh
+++ source/01_preflight_check.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 set -e
 
+# --- Load config ---
 source "config.env"
 
+# --- Check for root ---
+echo "Checking for root privileges..."
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Please run this script as root or use sudo."
+    exit 1
+fi
+
+echo "Root check complete!"
+
 # Check for requred packages before starting install
+echo "Checking for required packages..."
+
 for cmd in $REQUIRED_CMDS; do
     if ! command -v $cmd &>/dev/null; then
         echo "Missing required command: $cmd"
@@ -15,3 +28,5 @@ if [[ $MISSING -eq 1 ]]; then
     echo "Please install all required packages before running this script."
     exit 1
 fi
+
+echo "Required packages found!"
