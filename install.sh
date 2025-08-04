f440ac37af7e9e5d71ae770d0a76dd906a5a448c
k ostaN <pyotrthevampire@gmail.com>
2025-08-04 15:35:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -e 
 
-# Load config
+# --- Load config ---
 cp config.env source/config.env
 source "source/config.env"
 
-# Run install
-"source/01_package_check.sh"
-echo "Package check is complete!"
+# --- Run install ---
+# --- --- 01 --- ---
+echo "Running checks..."
+"source/01_preflight_check.sh"
+echo "Check is complete!"
+# --- --- 02 --- ---
 echo "Starting downloads now!"
 "source/02_downloads.sh"
 echo "Downloads complete!"
+# --- --- 03 --- ---
 echo "Setting up your device for chroot!"
 "source/03_setup.sh"
 echo "Setup complete!"
