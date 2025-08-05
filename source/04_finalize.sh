75362f8c5c46832aea8e7b8057bdecf034915ce4
k ostaN <pyotrthevampire@gmail.com>
2025-08-05 11:31:08 -0500

    Update 04_finalize.sh

--- source/04_finalize.sh
+++ source/04_finalize.sh
@@ -7,6 +7,10 @@ source "config.env"
 ROOT=/mnt/root
 BOOT=/mnt/root/boot
 
+# --- Detect PARTUUIDs ---
+PARTUUID_BOOT=$(blkid -s PARTUUID -o value ${DEVICE}1)
+PARTUUID_ROOT=$(blkid -s PARTUUID -o value ${DEVICE}2)
+
 # --- Set root password (hashed) ---
 echo "Setting root password..."
 ROOT_PASSWORD_SALT=$(openssl rand -base64 12)
@@ -18,18 +22,25 @@ sed -i "s|^${USER}:[^:]*:|${USER}:${ROOT_PASSWORD_HASHED}:|" "$ROOT/etc/shadow"
 echo "$HOSTNAME" > "$ROOT/etc/hostname"
 
 # --- Set keymap ---
-sed -i "s/^KEYMAP=.*/KEYMAP=\"$KEYMAP\"/" "$ROOT/etc/conf.d/keymaps"
+if [ ! -f "$ROOT/etc/conf.d/keymaps" ]; then
+    echo "KEYMAP=\"$KEYMAP\"" > "$ROOT/etc/conf.d/keymaps"
+else
+    sed -i "s/^KEYMAP=.*/KEYMAP=\"$KEYMAP\"/" "$ROOT/etc/conf.d/keymaps"
+fi
 
 # --- Add network symlink ---
 ln -sf net.lo "$ROOT/etc/init.d/net.end0"
 
-# --- Set up fstab ---
-echo "PARTUUID=6c586e13-01   /boot   vfat    defaults,auto,noatime,umask=0022,uid=0,gid=100   0 0" >> "$ROOT/etc/fstab"
+# --- Set up fstab (dynamic PARTUUIDs) ---
+cat <<EOF > "$ROOT/etc/fstab"
+PARTUUID=$PARTUUID_BOOT   /boot   vfat    defaults,auto,noatime,umask=0022,uid=0,gid=100   0 0
+PARTUUID=$PARTUUID_ROOT   /       btrfs   defaults,noatime,compress=zstd,ssd,space_cache,subvolid=5,subvol=/   0 1
+EOF
 
 # --- Update sshd config for root login ---
 sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/' "$ROOT/etc/ssh/sshd_config"
 
-# --- Optionally set up make.conf with parallelism and binhost ---
+# --- make.conf with parallelism and binhost ---
 cat <<EOF >> "$ROOT/etc/portage/make.conf"
 
 MAKEOPTS="-j$(nproc)"
@@ -38,7 +49,7 @@ FEATURES="\${FEATURES} getbinpkg"
 PORTAGE_BINHOST="https://dev.drassal.net/genpi64/pi64pie_20250115_binpkgs"
 EOF
 
-# --- Optionally add overlay repo configs ---
+# --- Overlay repo configs ---
 mkdir -p "$ROOT/etc/portage/repos.conf"
 cat <<EOF > "$ROOT/etc/portage/repos.conf/gentoo.conf"
 [DEFAULT]
@@ -63,9 +74,17 @@ priority = 100
 auto-sync = yes
 EOF
 
-# --- Optionally fix cmdline.txt (may require custom logic for your setup) ---
-CMDLINE="console=serial0,115200 console=tty1 dwc_otg.lpm_enable=0 root=PARTUUID=6c586e13-02 rootfstype=btrfs rootdelay=0 fsck.repair=yes rootwait"
+# --- Create cmdline.txt (dynamic PARTUUID) ---
+CMDLINE="console=serial0,115200 console=tty1 root=PARTUUID=$PARTUUID_ROOT rootfstype=btrfs rootdelay=0 fsck.repair=yes rootwait"
 echo "$CMDLINE" > "$BOOT/cmdline.txt"
+
+# --- Create config.txt ---
+cat <<EOF > "$BOOT/config.txt"
+arm_64bit=1
+enable_uart=1
+kernel=kernel8.img
+dtoverlay=disable-bt
+EOF
 
 # --- Unmount and clean up ---
 echo "Syncing and unmounting..."
